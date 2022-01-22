5ef6892ae134935858c731e366599678c26c31fa
ajomuch92 <aaronmontes_92@yahoo.es>
2022-01-22 13:35:00 -0600

    fix: 🐛 Fixing falsable on cell values
    This is on table component
    
    ✅ Closes: #1

--- src/lib-components/teTable.vue
+++ src/lib-components/teTable.vue
@@ -14,8 +14,8 @@
           <tr v-for="(item, key) in filteredItems" :key="key" :class="rowClass(key)" v-show="rowVisibility(key+1)">
             <td v-if="showRowNum">{{key+1}}</td>
             <td v-for="(header, index) in headers" :key="index" class="text-sm text-gray-900 font-medium px-6 whitespace-nowrap" :class="{...paddingClass, 'border-r': bordered}">
-              <slot :name="header.field||header" v-bind:value="item[header.field] || item[header]">
-                {{item[header.field] || item[header]}}
+              <slot :name="header.field||header" v-bind:value="getCellValue(item, header)">
+                {{getCellValue(item, header)}}
               </slot>
             </td>
           </tr>
@@ -177,6 +177,9 @@ export default {
         return index >= first && index <= last
       }
       return true;
+    },
+    getCellValue(item, header) {
+       return item[header.field] ?? item[header];
     }
   }
 }
