beb427797d88dbf36c30c7e84ef00731b8c330c5
ajomuch92 <aaronmontes_92@yahoo.es>
2022-07-31 10:07:13 -0600

    docs: ✏️ Updating documentation for toast

--- dev/views/Toast.vue
+++ dev/views/Toast.vue
@@ -36,6 +36,41 @@
         </script>
       </code>
     </pre>
+    <h1 class="text-lg my-1">Show a Message Globally</h1>
+    <div class="w-1/3 my-2">
+      <label class="w-1/3">Choose a type</label>
+      <te-select class="w-32" v-model="typeToastSelected" :options="typeToasts" />
+    </div>
+    <div class="flex space-x-2">
+      <te-button @click="showToast('bottom', 'left')">Bottom Start</te-button>
+      <te-button @click="showToast('bottom', 'center')">Bottom Center</te-button>
+      <te-button @click="showToast('bottom', 'right')">Bottom End</te-button>
+      <te-button @click="showToast('top', 'left')">Top Start</te-button>
+      <te-button @click="showToast('top', 'center')">Top Center</te-button>
+      <te-button @click="showToast('top', 'right')">Top End</te-button>
+    </div>
+    <pre class="language-markup">
+      <code>
+        <script type="prism-js">
+          const toast = this.$showToast({
+            position: 'bottom', // top
+            align: 'center', // left, right
+            timeout: 10000, // if undefined you can cloase manually
+            type: 'normal', //light
+            toast: { // same props to pass as a regular toast (normal o light) component
+              title: 'Title',
+              subtitle: 'subtitle',
+              message: 'Message',
+              color: 'normal', // primary, success, warning, danger
+              type: 'info', // success, warning, danger (only for light toast)
+            }
+          });
+          setTimeout(() => {
+            toast.close();
+          }, 1500)
+        </script>
+      </code>
+    </pre>
   </div>
 </template>
 
@@ -46,8 +81,26 @@ export default {
     colors: ['normal','primary', 'success', 'warning', 'danger'],
     color: 'primary',
     types: ['info', 'success', 'warning', 'danger'],
-    type: 'info'
-  })
+    typeToasts: ['normal', 'light'],
+    type: 'info',
+    typeToastSelected: 'normal'
+  }),
+  methods: {
+    showToast(position, align) {
+      this.$showToast({
+        position,
+        align,
+        timeout: 10000, // undefined
+        type: this.typeToastSelected, //light
+        toast: { // same props to pass as a regular toast (normal o light) component
+          title: 'Title',
+          subtitle: 'subtitle',
+          message: 'Message',
+          color: 'success'
+        }
+      });
+    }
+  }
 }
 </script>
 
