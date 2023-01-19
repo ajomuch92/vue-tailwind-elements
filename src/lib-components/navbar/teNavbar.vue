aabd2528f34a0c10547c5d4a10ba989005a7087a
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-01-18 19:17:11 -0600

    fix: 🐛 Fixing missing binding method

--- src/lib-components/navbar/teNavbar.vue
+++ src/lib-components/navbar/teNavbar.vue
@@ -25,7 +25,7 @@
         " :class="{'text-gray-500': !dark, 'white-text': dark}" type="button">
           <span @click="$emit('toggle-click', $event)">
             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars"
-              class="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              class="w-6" :class="{'white-text': dark}" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
               <path fill="currentColor"
                 d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
               </path>
@@ -44,7 +44,7 @@
             lg:mt-0
             mr-1
           "
-          :href="hrefHeader"
+          v-bind="headerTagBinds"
         >
           <slot name="brand" />
         </component>
