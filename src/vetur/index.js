20f3a6b4f1d84cfd4c65e3d78d8273667b8cb48d
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-01-18 18:20:02 -0600

    feat: 🎸 Adding all vetur files and adding home page

--- src/vetur/index.js
+++ src/vetur/index.js
@@ -2,69 +2,74 @@
 const fs = require('fs');
 const path = require('path');
 const kebabCase = require('kebab-case');
-const JSON5 = require('json5');
-
-const mainDir = path.resolve(__dirname, '../lib-components')
+const mainDir = path.resolve(__dirname, '../lib-components');
 
 let tags = {};
 let attributes = {};
 
-fs.readdirSync(mainDir, { withFileTypes: true })
-    .filter((file) => file.name.endsWith('.vue') && file.name.startsWith('te'))
-    .forEach(item => {
-      if (!item.isDirectory()) {
-        const componentName = item.name;
-        const name = kebabCase(componentName.replace('.vue', ''));
-        const componentJsPath = mainDir + '\\' + item.name
+fs.readdirSync(mainDir, { withFileTypes: true }).filter((file) => file.name === 'index.js').forEach((item) => {
+  const componentJsPath = mainDir + '\\' + item.name;
+  const file = fs.readFileSync(componentJsPath, 'utf-8');
+  const fileString = file.toString();
+  const filesPath = fileString.split('\n');
+  filesPath.forEach((path) => {
+    let singlePath = path.match(/'([^']+)'/)[0];
+    if (singlePath) {
+      singlePath = singlePath.replaceAll('\'', '').replace('.', '');
+      let name = singlePath.substring(singlePath.lastIndexOf('/') + 1);
+      name = kebabCase(name.replace('.vue', ''));
+      const componentJsPath = mainDir + singlePath;
+      const file = fs.readFileSync(componentJsPath, 'utf-8');
 
-        const file = fs.readFileSync(componentJsPath, 'utf-8');
+      const fileString = file.toString();
 
-        const fileString = file.toString();
+      let stringToken = 'export default {';
 
-        let stringToken = 'export default {';
+      const index = fileString.indexOf(stringToken);
+      const indexEnd = fileString.indexOf('</script>');
 
-        const index = fileString.indexOf(stringToken);
-        const indexEnd = fileString.indexOf('</script>');
+      const jsonString = fileString.substring(index + stringToken.length - 1 , indexEnd).trim()
+        .replaceAll('\n', '')
+        .replaceAll('\r', '')
+        .replaceAll(' ', '');
+        
+      stringToken = 'props:';
+      const indexOfProp = jsonString.indexOf(stringToken);
 
-        const jsonString = fileString.substring(index + stringToken.length - 1 , indexEnd).trim()
-          .replaceAll('\n', '')
-          .replaceAll('\r', '')
-          .replaceAll(' ', '');
-          
-        stringToken = 'props:';
-        const indexOfProp = jsonString.indexOf(stringToken);
+      let substringProps = jsonString.substring(indexOfProp + stringToken.length);
 
-        let substringProps = jsonString.substring(indexOfProp + stringToken.length);
+      substringProps = analyzeText(substringProps);
 
-        substringProps = analyzeText(substringProps);
-
-        const splittedSubStringProps = substringProps.split('},');
-        const attributesArr = [];
-        for (let i = 0; i < splittedSubStringProps.length; i += 1) {
-          const currentString = splittedSubStringProps[i];
-          const substringKey = currentString.substring(0, currentString.indexOf(':'));
-          if (substringKey) {
-            attributesArr.push(substringKey);
-            stringToken = 'type:';
-            const typeIndex = currentString.indexOf(stringToken);
-            const commaIndex = currentString.indexOf(',', typeIndex);
-            const attributeType = currentString.substring(typeIndex + stringToken.length, commaIndex);
-            attributes[`${name}/${substringKey}`] = {
-              description: '',
-              type: attributeType.replace('{type:', '').replace('[', '')
-            }
+      const splittedSubStringProps = substringProps.split('},');
+      const attributesArr = [];
+      for (let i = 0; i < splittedSubStringProps.length; i += 1) {
+        const currentString = splittedSubStringProps[i];
+        const substringKey = currentString.substring(0, currentString.indexOf(':'));
+        if (substringKey) {
+          attributesArr.push(substringKey);
+          if (substringKey !== kebabCase(substringKey)) {
+            attributesArr.push(kebabCase(substringKey));
+          }
+          stringToken = 'type:';
+          const typeIndex = currentString.indexOf(stringToken);
+          const commaIndex = currentString.indexOf(',', typeIndex);
+          const attributeType = currentString.substring(typeIndex + stringToken.length, commaIndex);
+          attributes[`${name}/${substringKey}`] = {
+            description: '',
+            type: attributeType.replace('{type:', '').replace('[', '')
           }
         }
-        
-        let mainTag = {
-          "attributes": attributesArr,
-          "description": `${name} component`,
-          "subtags": []
-        }
-        tags[name] = mainTag;
       }
+      
+      let mainTag = {
+        "attributes": attributesArr,
+        "description": `${name} component`,
+        "subtags": []
+      }
+      tags[name] = mainTag;
     }
-);
+  })
+})
 
 
 function analyzeText(text) {
