7ad17c504e7e8ac8d22ffe5f79ba47108d99c5ed
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-02-09 19:41:58 -0600

    feat: 🎸 Adding new chip component

---
+++ src/lib-components/teChip.vue
@@ -0,0 +1,85 @@
+<template>
+  <span class="
+    px-4
+    py-2
+    rounded-full
+    font-semibold
+    text-sm
+    flex
+    align-center
+    w-max"
+    :class="[colorClass, sizeClass]"
+  >
+    <img v-if="imgUrl" class="rounded-full w-9 h-9 max-w-none" :src="imgUrl" :class="[sizeClass, widthImgClass]" />
+    <slot />
+    <button v-if="closable" class="bg-transparent hover focus:outline-none">
+      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times"
+        class="w-3 ml-3" role="img" xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 352 512">
+        <path fill="currentColor"
+          d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z">
+        </path>
+      </svg>
+    </button>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'TeChip',
+  props: {
+    type: {
+      type: String,
+      default: 'light',
+      validator: (value) => ['normal', 'primary', 'secondary', 'success', 'warning', 'danger', 'pink', 'purple', 'light', 'dark'].includes(value)
+    },
+    size: {
+      type: String,
+      default: 'medium',
+      validator: (value) => ['small', 'medium','large'].includes(value)
+    },
+    closable: {
+      type: Boolean,
+      default: false,
+    },
+    imgUrl: {
+      type: String,
+      default: '',
+    }
+  },
+  computed: {
+    colorClass() {
+      return {
+        'bg-gray-500 text-white': this.type === 'normal',
+        'bg-indigo-500 text-white': this.type === 'secondary',
+        'bg-green-500 text-white': this.type === 'success',
+        'bg-blue-500 text-white': this.type === 'primary',
+        'bg-yellow-500 text-white': this.type === 'warning',
+        'bg-red-500 text-white': this.type === 'danger',
+        'bg-purple-500 text-white': this.type === 'purple',
+        'bg-pink-500 text-white': this.type === 'pink',
+        'bg-gray-800 text-white': this.type === 'dark',
+        'bg-gray-200 text-black': this.type === 'light',
+      };
+    },
+    sizeClass() {
+      return {
+        'h-9': this.size === 'small',
+        'h-11': this.size === 'medium',
+        'h-14': this.size === 'large',
+      };
+    },
+    widthImgClass() {
+      return {
+        'w-9': this.size === 'small',
+        'w-11': this.size === 'medium',
+        'w-14': this.size === 'large',
+      };
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
