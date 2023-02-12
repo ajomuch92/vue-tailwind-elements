14cd23c6512dade6126e4cba6ab5998979c23447
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-02-11 19:39:16 -0600

    feat: 🎸 Creating code view component to use it into project

---
+++ dev/components/CodeView.vue
@@ -0,0 +1,49 @@
+<template>
+  <pre :class="`language-${lang} relative`">
+    <code>
+      {{ code }}
+    </code>
+    <te-button class="absolute top-5 right-5" icon @click="copy">
+      <te-icon name="clipboard"/>
+    </te-button>
+  </pre>
+</template>
+
+<script>
+
+export default {
+  name: 'CodeView',
+  props: {
+    code: {
+      type: String,
+      required: true,
+    },
+    lang: {
+      type: String,
+      default: 'markup'
+    }
+  },
+  methods: {
+    copy() {
+      navigator.clipboard.writeText(this.code);
+      this.$showToast(
+        {
+          position: 'bottom',
+          align: 'right',
+          timeout: 1000,
+          type: 'normal',
+          toast: {
+            title: 'Copied',
+            message: 'Code copied successfully',
+            color: 'success',
+          }
+        }
+      );
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
