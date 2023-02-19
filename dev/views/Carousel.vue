d9bfd32c8325c87d48ef33ec0a3b39c081faa1af
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-02-19 13:55:48 -0600

    feat: 🎸 Updating example for carousel

--- dev/views/Carousel.vue
+++ dev/views/Carousel.vue
@@ -2,13 +2,19 @@
   <div class="p-6">
     <h1 class="text-3xl my-2">Carousel</h1>
     <h1 class="text-lg">Example</h1>
-    <te-carousel>
+    <te-carousel :fade="fade" :loop="loop" :interval="interval">
       <te-carousel-item v-for="(image, key) in images2" :key="key" :img-url="image" :title="`Slide ${key+1}`">
         <p class="text-base bg-red-200 p-3">
           This is Slot
         </p>
       </te-carousel-item>
     </te-carousel>
+    <div class="flex items-center mt-2">
+      <te-checkbox v-model="fade" class="mx-2">Fade</te-checkbox>
+      <te-checkbox v-model="loop" class="mx-2">Loop</te-checkbox>
+      <label class="mr-1">Interval:</label>
+      <te-input placeholder="Interval" v-model.number="interval"></te-input>
+    </div>
     <code-view :code="code[0]" />
     <p>
       <b>Note:</b> Use images with same size to avoid wrong behavior.
@@ -22,6 +28,9 @@ export default {
   components: { CodeView },
   name: 'CarouselEx',
   data: () => ({
+    loop: false,
+    interval: 1500,
+    fade: false,
     images2: [
       'https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg',
       'https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg',
@@ -29,7 +38,7 @@ export default {
     ],
     code: [
       `
-      <te-carousel>
+      <te-carousel :fade="fade" :loop="loop" :interval="interval">
         <te-carousel-item v-for="(image, key) in images2" :key="key" :img-url="image" :title="...">
           <p class="text-base bg-red-200 p-3">
             This is Slot
