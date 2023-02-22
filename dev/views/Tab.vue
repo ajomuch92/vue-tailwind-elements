d49e178fb84fafda1cbbbccff8ead337ccd0672f
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-02-21 18:11:36 -0600

    feat: 🎸 Adding example for notification on tab component

--- dev/views/Tab.vue
+++ dev/views/Tab.vue
@@ -2,7 +2,7 @@
   <div class="p-6">
     <h1 class="text-3xl my-2">Tabs</h1>
     <h1 class="text-lg my-1">Normal</h1>
-    <te-tabs v-model="selectedTab" :titles="['Home', 'Profile', 'Messages', {label: 'Contact', disabled: true}]">
+    <te-tabs v-model="selectedTab" :titles="['Home', 'Profile', 'Messages', {label: 'Contact', disabled: true}, {label: 'Notification', notification: { color: 'secondary', text: '+2'}}]">
       <template slot="tab-1">
         Tab content 1
       </template>
@@ -14,6 +14,9 @@
       </template>
       <template slot="tab-4">
         Tab content 4
+      </template>
+      <template slot="tab-5">
+        Notifications
       </template>
     </te-tabs>
     <p class="my-2">Selected tab: {{selectedTab}}</p>
@@ -65,6 +68,7 @@
         Tab content 4
       </template>
     </te-tabs>
+    <code-view :code="code[3]" />
   </div>
 </template>
 
@@ -111,6 +115,22 @@ export default {
       `,
       `
       <te-tabs :titles="['Home', 'Profile', 'Messages', 'Contact']" vertical>
+        <template slot="tab-1">
+          Tab content 1
+        </template>
+        <template slot="tab-2">
+          Tab content 2
+        </template>
+        <template slot="tab-3">
+          Tab content 3
+        </template>
+        <template slot="tab-4">
+          Tab content 4
+        </template>
+      </te-tabs>
+      `,
+      `
+      <te-tabs :titles="['Home', 'Profile', 'Messages', 'Contact']" pills>
         <template slot="tab-1">
           Tab content 1
         </template>
