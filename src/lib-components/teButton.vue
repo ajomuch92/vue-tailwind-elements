3777496054c74e5a1ff51d9620b9f38f4b0c265e
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-03-03 20:16:16 -0600

    feat: 🎸 Adding plugin to use with button to simplify component

--- src/lib-components/teButton.vue
+++ src/lib-components/teButton.vue
@@ -1,15 +1,7 @@
 <template>
   <button
     ref="button"
-    class="
-        font-bold
-        outline-none
-        focus:outline-none
-        ease-linear
-        transition-all
-        duration-150
-        disabled:opacity-50
-      "
+    class="btn"
     :class="getClass"
     :disabled="disabled"
     :type="buttonType"
@@ -86,56 +78,25 @@ export default {
   },
   computed: {
     getClass() {
+      const sizes = {
+        small: 'md',
+        medium: 'lg',
+        large: 'lg'
+      };
       return {
-        'bg-gray-500 hover:bg-gray-700 active:bg-gray-500 text-white': this.type === 'normal' && !this.onlyText && !this.outlined,
-        'bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-500 text-white': this.type === 'secondary' && !this.onlyText && !this.outlined,
-        'bg-green-500 hover:bg-green-700 active:bg-green-500 text-white': this.type === 'success' && !this.onlyText && !this.outlined,
-        'bg-blue-500 hover:bg-blue-700 active:bg-blue-500 text-white': this.type === 'primary' && !this.onlyText && !this.outlined,
-        'bg-yellow-500 hover:bg-yellow-700 active:bg-yellow-500 text-white': this.type === 'warning' && !this.onlyText && !this.outlined,
-        'bg-red-500 hover:bg-red-700 active:bg-red-500 text-white': this.type === 'danger' && !this.onlyText && !this.outlined,
-        'bg-purple-500 hover:bg-purple-700 active:bg-purple-500 text-white': this.type === 'purple' && !this.onlyText && !this.outlined,
-        'bg-pink-500 hover:bg-pink-700 active:bg-pink-500 text-white': this.type === 'pink' && !this.onlyText && !this.outlined,
-        'bg-gray-800 hover:bg-gray-900 active:bg-gray-800 text-white': this.type === 'dark' && !this.onlyText && !this.outlined,
-        'bg-gray-200 hover:bg-gray-300 active:bg-gray-300 text-black': this.type === 'light' && !this.onlyText && !this.outlined,
-        'background-transparent text-gray-500': this.type === 'normal' && this.onlyText && !this.outlined,
-        'background-transparent text-indigo-500': this.type === 'secondary' && this.onlyText && !this.outlined,
-        'background-transparent text-green-500': this.type === 'success' && this.onlyText && !this.outlined,
-        'background-transparent text-blue-500': this.type === 'primary' && this.onlyText && !this.outlined,
-        'background-transparent text-yellow-500': this.type === 'warning' && this.onlyText && !this.outlined,
-        'background-transparent text-red-500': this.type === 'danger' && this.onlyText && !this.outlined,
-        'background-transparent text-purple-500': this.type === 'purple' && this.onlyText && !this.outlined,
-        'background-transparent text-pink-500': this.type === 'pink' && this.onlyText && !this.outlined,
-        'background-transparent text-gray-800': this.type === 'dark' && this.onlyText && !this.outlined,
-        'background-transparent text-gray-200': this.type === 'light' && this.onlyText && !this.outlined,
-        'bg-transparent border border-solid border-gray-500 text-gray-500 hover:bg-gray-500 hover:text-white': this.type === 'normal' && this.outlined,
-        'bg-transparent border border-solid border-indigo-500 text-indigo-500 hover:bg-indigo-500 hover:text-white': this.type === 'secondary' && this.outlined,
-        'bg-transparent border border-solid border-green-500 text-green-500 hover:bg-green-500 hover:text-white': this.type === 'success' && this.outlined,
-        'bg-transparent border border-solid border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white': this.type === 'primary' && this.outlined,
-        'bg-transparent border border-solid border-yellow-500 text-yellow-500 hover:bg-yellow-500 hover:text-white': this.type === 'warning' && this.outlined,
-        'bg-transparent border border-solid border-red-500 text-red-500 hover:bg-red-500 hover:text-white': this.type === 'danger' && this.outlined,
-        'bg-transparent border border-solid border-purple-500 text-purple-500 hover:bg-purple-500 hover:text-white': this.type === 'purple' && this.outlined,
-        'bg-transparent border border-solid border-pink-500 text-pink-500 hover:bg-pink-500 hover:text-white': this.type === 'pink' && this.outlined,
-        'bg-transparent border border-solid border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white': this.type === 'dark' && this.outlined,
-        'bg-transparent border border-solid border-gray-200 text-gray-200 hover:bg-gray-200 hover:text-black': this.type === 'light' && this.outlined,
+        [this.type]: true,
+        [this.size]: true,
+        'only-text': this.onlyText && !this.outlined,
+        'outlined': this.outlined,
         'rounded': !this.rounded && !this.noRounded,
         'rounded-full': this.rounded && !this.noRounded,
         'uppercase': this.uppercased,
         'shadow': this.shadowed,
-        'hover:shadow-md': this.shadowed && this.size === 'small',
-        'hover:shadow-lg': this.shadowed && this.size === 'medium',
-        'hover:shadow-lg': this.shadowed && this.size === 'large',
-        'text-xs py-1 h-8': this.size === 'small',
-        'text-sm py-2 h-10': this.size === 'medium',
-        'text-base py-3 h-12': this.size === 'large',
+        [`hover:shadow-${sizes[this.size]}`]: this.shadowed,
         'cursor-not-allowed opacity-50': this.disabled,
         'opacity-50 cursor-not-allowed': this.loading,
         'relative overflow-hidden': this.ripple,
-        'px-4': this.size === 'small' && !this.icon,
-        'px-6': this.size === 'medium' && !this.icon,
-        'px-8': this.size === 'large' && !this.icon,
-        'px-1 w-8': this.size === 'small' && this.icon,
-        'px-2 w-10': this.size === 'medium' && this.icon,
-        'px-3 w-12': this.size === 'large' && this.icon,
+        'icon': this.icon,
       }
     }
   },
--- src/plugin/index.js
+++ src/plugin/index.js
@@ -1,8 +1,11 @@
 const styles = require('./styles');
 const plugin = require('tailwindcss/plugin');
+const buttons = require( './buttons');
 
 module.exports = plugin(
+  ({ addComponents, theme }) => {
     addComponents(styles);
+    addComponents(buttons(theme));
   },
   {}
 );
