33076dff19832f0475cd50ac58ba3380891fbac4
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-03-04 19:32:30 -0600

    feat: 🎸 Adding select config

--- src/plugin/inputs.js
+++ src/plugin/inputs.js
@@ -34,7 +34,46 @@ module.exports = function (theme) {
       },
     }
   };
+  const select = {
+    'select.form-select': {
+      minWidth: '150px',
+      appearance: 'none',
+      display: 'block',
+      fontWeight: theme('fontWeight.normal'),
+      color: theme('colors.gray.700'),
+      backgroundColor: theme('colors.white'),
+      backgroundClip: 'padding-box',
+      backgroundRepeat: 'no-repeat',
+      borderWidth: theme('borderWidth.DEFAULT'),
+      borderStyle: 'solid',
+      borderColor: theme('colors.gray.300'),
+      borderRadius: theme('borderRadius.DEFAULT'),
+      transitionTimingFunction: theme('transitionTimingFunction.DEFAULT'),
+      transitionDuration: theme('transitionDuration.150'),
+      transitionProperty: theme('transitionProperty.DEFAULT'),
+      margin: theme('margin.0'),
+      '&:focus': {
+        color: theme('colors.gray.700'),
+        backgroundColor: theme('colors.white'),
+        borderColor: theme('colors.blue.600'),
+        outline: theme('outline.none'),
+      },
+      '&.small': {
+        padding: `${theme('padding.1')} ${theme('padding.2')}`,
+        fontSize: theme('fontSize.sm'),
+      },
+      '&.medium': {
+        padding: `${theme('padding[1.5]')} ${theme('padding.3')}`,
+        fontSize: theme('fontSize.base'),
+      },
+      '&.large': {
+        padding: `${theme('padding.2')} ${theme('padding.4')}`,
+        fontSize: theme('fontSize.xl'),
+      },
+    }
+  };
   return {
     ...file,
+    ...select,
   };
 }
