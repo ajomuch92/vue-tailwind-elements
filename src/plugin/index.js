4c9e8add282044e7bcadc2df25662a1f4c5f95f6
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-03-04 19:32:10 -0600

    feat: 🎸 Adding select file config for plugin

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -2,12 +2,14 @@ const styles = require('./styles');
 const plugin = require('tailwindcss/plugin');
 const buttons = require( './buttons');
 const pagination = require('./pagination');
+const inputs = require('./inputs');
 
 module.exports = plugin(
   ({ addComponents, theme }) => {
     addComponents(styles);
     addComponents(buttons(theme));
     addComponents(pagination(theme));
+    addComponents(inputs(theme));
   },
   {}
 );
