5cd29b76b39845f4ec1142b35a2c153201a07830
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-03-06 16:40:22 -0600

    docs: ✏️ Updating example for alert to show global alert method

--- dev/views/Alert.vue
+++ dev/views/Alert.vue
@@ -20,6 +20,23 @@
     <te-alert text="Info" type="info" solid />
     <te-alert text="Warning" type="warning" solid />
     <te-alert text="Danger" type="danger" solid />
+    <h1 class="text-lg my-1">Show a Alert Globally</h1>
+    <div class="w-1/3 my-2">
+      <label class="w-1/3">Choose a type</label>
+      <te-select class="w-32" v-model="alertTypeSelected" :options="alertTypes" />
+      <te-checkbox v-model="solid">Solid?</te-checkbox><br>
+      <label>Text</label>
+      <te-input v-model="message" />
+    </div>
+    <div class="flex space-x-2">
+      <te-button @click="showAlert('bottom', 'left')">Bottom Start</te-button>
+      <te-button @click="showAlert('bottom', 'center')">Bottom Center</te-button>
+      <te-button @click="showAlert('bottom', 'right')">Bottom End</te-button>
+      <te-button @click="showAlert('top', 'left')">Top Start</te-button>
+      <te-button @click="showAlert('top', 'center')">Top Center</te-button>
+      <te-button @click="showAlert('top', 'right')">Top End</te-button>
+    </div>
+    <code-view :code="code4" lang="js" />
   </div>
 </template>
 
@@ -30,6 +47,10 @@ export default {
   name: 'Alert',
   components: { CodeView },
   data: () => ({
+    alertTypes: ['normal','primary', 'success', 'warning', 'danger'],
+    alertTypeSelected: 'normal',
+    solid: false,
+    message: 'Hello World!',
     code: '<te-alert text="Hello world" />',
     code2: `
       <te-alert text="Default" />
@@ -46,8 +67,30 @@ export default {
       <te-alert text="Info" type="info" solid />
       <te-alert text="Warning" type="warning" solid />
       <te-alert text="Danger" type="danger" solid />
+    `,
+    code4: `
+      this.$showAlert({
+        position,
+        align,
+        solid: this.solid,
+        timeout: 3000,
+        type: this.alertTypeSelected,
+        text: this.message
+      });
     `
-  })
+  }),
+  methods: {
+    showAlert(position, align) {
+      this.$showAlert({
+        position,
+        align,
+        solid: this.solid,
+        timeout: 3000,
+        type: this.alertTypeSelected,
+        text: this.message
+      });
+    }
+  }
 }
 </script>
 
