9b02b108b912f8299d1c2e13c808e82a015164fb
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-03-06 16:40:39 -0600

    fix: 🐛 Removing validation

--- src/lib-components/alert.js
+++ src/lib-components/alert.js
@@ -3,7 +3,6 @@ import TeToastHandler from './toastHandler.vue';
 
 const showAlert = (options) => {
   const { type, position, text, solid, align, timeout } = options || {};
-  if (!toast) return;
   const ToastClass = Vue.extend(TeToastHandler);
   const instance = new ToastClass({
     propsData: {
