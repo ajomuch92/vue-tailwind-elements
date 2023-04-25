9ae4a1b0a6f5b9aee19382979e70c9763d153dd1
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-04-24 20:54:52 -0600

    refactor: 💡 Updating tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './dev/**/*.{vue,js,ts}', ...(process.env.NODE_ENV === 'production' ? [] : ['./dev/**/*.{vue,js,ts}'])],
+  content: ['./index.html', './dev/**/*.{vue,js,ts}', ...(process.env.NODE_ENV !== 'production' ? [] : ['./src/**/*.{vue,js,ts}'])],
   theme: {
     extend: {},
   },
